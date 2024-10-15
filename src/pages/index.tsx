8a7d710395e347cec8d0a540ea43ec68397b1bba
John <john4button@gmail.com>
2024-10-15 09:50:31 -0400

    call localhost

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import { useRouter } from 'next/router';
 import { ClipLoader } from 'react-spinners';
 import Link from 'next/link';
 
-export default function Index({displayData, setDisplayData}:any) {
-  const [input, setInput] = useState<string>("")  
+export default function Index({ displayData, setDisplayData }: any) {
+  const [input, setInput] = useState<string>("")
   const [loading, setLoading] = useState<boolean>(false)
   const router = useRouter()
 
@@ -16,17 +16,17 @@ export default function Index({displayData, setDisplayData}:any) {
     borderColor: "#0ea5e9",
   };
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     setLoading(true)
-    try{
-      const response = await axios.post("http://18.219.145.113:5000/",
-      {input: input}).then(res => res.data)
-      .catch(err => console.log(err))
+    try {
+      const response = await axios.post("localhost:5000",
+        { input: input }).then(res => res.data)
+        .catch(err => console.log(err))
       setDisplayData(response)
       setLoading(false)
       router.push('/results')
     }
-    catch(err){
+    catch (err) {
       console.log(err)
       setLoading(false)
     }
@@ -34,17 +34,17 @@ export default function Index({displayData, setDisplayData}:any) {
 
   return (
     <main className="relative w-screen h-screen flex flex-col justify-center px-40">
-      {loading && 
-      <div className="absolute inset-0 w-full h-full bg-white/[0.7] flex items-center justify-center text-black text-7xl font-bold z-[500]">
-        <ClipLoader
-        color={"transparent"}
-        loading={loading}
-        cssOverride={override}
-        size={200}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
-      </div>}
+      {loading &&
+        <div className="absolute inset-0 w-full h-full bg-white/[0.7] flex items-center justify-center text-black text-7xl font-bold z-[500]">
+          <ClipLoader
+            color={"transparent"}
+            loading={loading}
+            cssOverride={override}
+            size={200}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>}
       <div className="flex flex-col items-start gap-8 w-[70%] text-black" data-aos="fade-up">
         <TypeAnimation
           sequence={[
@@ -53,17 +53,17 @@ export default function Index({displayData, setDisplayData}:any) {
           ]}
           wrapper="span"
           speed={50}
-          style={{ fontSize: '3.75em', fontWeight: '700', display: 'inline-block', pointerEvents:"none"}}
+          style={{ fontSize: '3.75em', fontWeight: '700', display: 'inline-block', pointerEvents: "none" }}
           repeat={Infinity}
         />
         <Link className="mt-[-1.5rem] text-2xl ml-1 font-normal hover:underline hover:text-sky-700 transition underline"
-        href="/info">
+          href="/info">
           Emotion detection using NLP-based sentiment analysis & transformer models
         </Link>
         <textarea className="p-8 text-2xl font-medium rounded-md h-[350px] max-h-[400px] min-h-[300px] 
         bg-sky-50/[0.8] w-full"
-        maxLength={10000} value={input} onChange={e => {setInput(e.target.value)}}
-        placeholder={'The sky is blue and the grass is green!'}/>
+          maxLength={10000} value={input} onChange={e => { setInput(e.target.value) }}
+          placeholder={'The sky is blue and the grass is green!'} />
         <button className="bg-sky-600 hover:bg-sky-500 text-white rounded-md px-16 
           py-[0.35rem] text-2xl font-medium transition-all"
           onClick={handleSubmit}>
